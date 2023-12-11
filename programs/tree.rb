b9c5c6a9d318824697365a7594b80c0061a01ac8
nobu09 <11857481+nobu09@users.noreply.github.com>
2023-12-11 09:33:22 +0900

    雪を降らせてみる

--- programs/tree.rb
+++ programs/tree.rb
@@ -3,17 +3,12 @@ def setup
   $ySlide = 10
   $split = 60
   $colors = ["green", "mediumaquamarine", "lightgreen", "turquoise", "limegreen"]
+  $snowflakes = []
   
   createCanvas($xSlide * $split, $ySlide * $split)
   background("black")
-end
-
-def draw
-  noLoop
   noStroke
   
-  fill("green")
-
   # スタートの座標位置
   xIndex = $split / 2
   yIndex = 0
@@ -32,8 +27,6 @@ def draw
     yIndex += 1
   end
 
-  fill("mediumseagreen")
-
   # 逆三角形を描いていく
   xIndex = $split / 2 - 0.5
   yIndex = 1
@@ -49,5 +42,43 @@ def draw
 
     xIndex -= 0.5
     yIndex += 1
+  end
+  
+  frameRate(30);
+end
+
+def draw
+  fill("white")
+  time = frameCount / 60
+
+  for i in 0..rand(3)
+    $snowflakes.push(Snowflake.new)
+  end
+
+  for flake in $snowflakes
+    flake.update(time)
+    flake.display()
+  end
+end
+
+class Snowflake
+  def initialize()
+    @posX = 0
+    @posY = rand(-50, 0)
+    @initialAngle = rand(0, 2 * PI)
+    @size = rand(3, 10)
+    
+    @radius = sqrt(rand(pow(width / 2, 2)));
+  end
+
+  def update(time)
+    w = 0.6
+    angle = w * time + @initialAngle
+    @posX = width / 2 + (@radius * sin(angle))
+    @posY += pow(@size, 0.5)
+  end
+
+  def display
+    ellipse(@posX, @posY, @size)
   end
 end
